f108fd3f0871fb33d9c4d6d57cf7f2271048258a
Anastasia <n41111215@mail.ru>
2020-05-06 15:12:02 +0300

    Добавила код версия 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        long bill = 200;
-        long amount = 1300;
-        long total;
-        if (amount > 1001) {
+        int bill = 200;
+        int amount = 1001;
+        int total;
+        if (amount >= 1001) {
             int bonus = (int) amount / 100;
             total = bonus + amount + bill;
         } else {
